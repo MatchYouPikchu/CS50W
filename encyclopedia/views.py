9b04f304d15c2e6ae4bca621ca711eb64356e370
MatchYouPikchu <m.wawruszczak@gmail.com>
2021-12-08 23:59:37 +0100

    First commit

---
+++ encyclopedia/views.py
@@ -0,0 +1,32 @@
+from django.http.response import HttpResponse, HttpResponseNotFound
+from django.shortcuts import redirect, render
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def title(request, title):
+    if (util.get_entry(title)):
+        return render(request,"encyclopedia/title.html" ,{
+            "title" : title,
+            "entry" : util.get_entry(title)
+            })
+    else:
+        return HttpResponse("Requested page was not found")
+     
+
+def search(request):
+    if request.method =='GET': 
+        title = request.GET['q']
+        if (util.get_entry(title)):
+            return render(request,"encyclopedia/title.html" ,{
+            "title" : title,
+            "entry" : util.get_entry(title)
+            })
+        else:
+            return HttpResponse("Requested page was not found")
+
